7f2bfa25a3bb4d62086cd7731f494c65da1a85b7
srishti <srishtiprasad500@gmail.com>
2022-08-05 22:41:50 +0530

    update

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT || 5000
+//change
 const start = async () => {
   try {
     //await connectDB(process.env.MONGO_URL)
